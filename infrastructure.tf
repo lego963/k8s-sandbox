053ee7507655759872df6820b1fafed2f6463009
Rodion Gyrbu <fpsoff@outlook.com>
2022-05-08 18:24:30 +0300

    Add cce cluster resource

--- infrastructure.tf
+++ infrastructure.tf
@@ -19,6 +19,13 @@ resource "opentelekomcloud_vpc_subnet_v1" "this" {
 }
 
 resource "opentelekomcloud_cce_cluster_v3" "this" {
-  name =
+  name        = "${var.project}-cce"
+  description = "K8s sandbox for ${var.project} project"
 
+  cluster_type = "VirtualMachine"
+  flavor_id    = var.cce_flavor
+
+  vpc_id                 = opentelekomcloud_vpc_v1.this.id
+  subnet_id              = opentelekomcloud_vpc_subnet_v1.this.network_id
+  container_network_type = "vpc-router"
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "project" {
 variable "vpc_cidr" {
   default = "192.168.0.0/16"
 }
+
+variable "cce_flavor" {
+  default = "cce.s1.small"
+}
